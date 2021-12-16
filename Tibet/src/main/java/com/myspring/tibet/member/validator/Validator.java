b6aa5da1d3a81358492b3a1d39a350faa2b1361e
gg165128 <94415501+gg165128@users.noreply.github.com>
2021-12-16 12:21:03 +0900

    tktktk

---
+++ Tibet/src/main/java/com/myspring/tibet/member/validator/Validator.java
@@ -0,0 +1,11 @@
+/*
+ * package com.myspring.tibet.member.validator;
+ * 
+ * import org.springframework.validation.Errors;
+ * 
+ * public interface Validator {
+ * 
+ * boolean supports(Class<?> clazz); void validate(Object target,Errors errors);
+ * 
+ * }
+ */
